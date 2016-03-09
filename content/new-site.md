61a5669d128a7a887a151550499916bc8b785f29
Rick Ree <github@rree.fastmail.fm>
2016-03-09 00:56:59 -0600

    wip

--- content/new-site.md
+++ content/new-site.md
@@ -2,5 +2,5 @@ Title: New Pelican site
 Date: Wednesday, 09 March 2016
 
 Old Wordpress site got hacked and redirected to a Russian porn
-site... of course, I was notified of this by a prospective
-student. Static site generation seems like the way to go from now on.
+site, and of course, I was notified of this by a prospective
+student. Static site generation from now on...
--- publishconf.py
+++ publishconf.py
@@ -13,10 +13,10 @@ from pelicanconf import *
 SITEURL = 'http://reelab.net/home'
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+## FEED_ALL_ATOM = 'feeds/all.atom.xml'
+## CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
-DELETE_OUTPUT_DIRECTORY = True
+## DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
